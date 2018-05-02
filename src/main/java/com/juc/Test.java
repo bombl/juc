819e806433323678d419fdb4733f8c094bdf3273
wangshuai@aladingBank.com <wangshuai@aladingBank.com>
2018-05-02 11:20:34 +0800

    first commit

---
+++ src/main/java/com/juc/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 文件名称:          	Test
+ * 版权所有@ 2017-2018 	魔谐(上海)软件有限公司，保留所有权利
+ * 编译器:           	JDK1.8
+ */
+
+package com.juc;
+
+/**
+ * TODO: 文件注释
+ * <p>
+ * Version		1.0.0
+ *
+ * @author wangshuai
+ * <p>
+ * Date	      2018/5/2 9:39
+ */
+public class Test {
+}
